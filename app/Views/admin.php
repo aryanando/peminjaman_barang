decd1c15a255183b6995455bdb0318dc8b7700e7
Aryanando <aryanando232@gmail.com>
2023-06-16 12:29:00 +0700

    try fix js

--- app/Views/admin.php
+++ app/Views/admin.php
@@ -255,7 +255,7 @@
     <script src="/assets/js/bootstrap.bundle.min.js"></script>
     <script src="/assets/js/jquery-3.6.0.min.js"></script>
     <script src="/assets/js/jquery.dataTables.min.js"></script>
-    <script src="/assets/js/chart.min.js"></script>
+    <script src="/assets/js/Chart.min.js"></script>
     <script>
         $('.count').each(function() {
             var $this = $(this);
